fcbd34c80a2348bd898b1938de47810f59b5f31d
Deb Luiza <99653788+DebLuiza@users.noreply.github.com>
2025-02-03 15:28:01 -0300

    alterações calendário

--- Clock/src/Components/Calendar/Calendar.jsx
+++ Clock/src/Components/Calendar/Calendar.jsx
@@ -3,7 +3,7 @@ import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import styles from "./Styles/Calendar.module.scss"
-
+import "./Styles/Calendar.scss"
 const localizer = momentLocalizer(moment);
 
 const events = [
